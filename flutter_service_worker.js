36889585b673fcf070b455ee76235bb31493fbbc
mycoffeehub <141278711+mycoffeehub@users.noreply.github.com>
2024-05-09 03:50:23 -0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/AssetManifest.smcbin": "a3d922a0b0bb96200631d1d9f758ce7d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "5b739b930c18a9e913ef9e00dab0a832",
+"assets/fonts/MaterialIcons-Regular.otf": "18d68117cc59b9121798f9ccbd7f0da8",
 "assets/NOTICES": "bdd87585deefb6e04728f796091ef299",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
@@ -23,11 +23,11 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "19d8617290c4390895dcbe11c095a929",
-"/": "19d8617290c4390895dcbe11c095a929",
-"main.dart.js": "a54536ffe18eb1cc9baa072b738393c7",
-"manifest.json": "401f08ffca48469a5d5a6f397741c417",
-"version.json": "6e3f48814f46d924be46f1a36c8d6b61"};
+"index.html": "14acf4caf7c2a05ba59f5f58c350e21e",
+"/": "14acf4caf7c2a05ba59f5f58c350e21e",
+"main.dart.js": "b9eaea82d6d5e2b51d2505031a04a737",
+"manifest.json": "21da7223e68cda6421b1f5f808ef65e5",
+"version.json": "2887ad490afeeaabd6aeea1db88e18b7"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
